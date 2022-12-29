e0f16935c934250db2eac403f53ece8cd665674a
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2022-12-28 22:08:42 -0800

    adde user home

--- lib/screens/userHomeScreen/user_home_screen.dart
+++ lib/screens/userHomeScreen/user_home_screen.dart
@@ -6,7 +6,43 @@ class UserHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('home screen'),),
+      backgroundColor: Color(0xffE7EFFA),
+      appBar: AppBar(
+        title: Text('Paniyaal'),
+        backgroundColor: Color(0xffdb3244),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.favorite),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 182,
+                height: 207,
+                child: Card(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Carpenter',
+                        style: TextStyle(fontWeight: FontWeight.w400),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
