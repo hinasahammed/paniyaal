848c5b9bd241ad742090860f885594ee99569d2c
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2023-01-11 21:42:33 -0800

    d

---
+++ lib/screens/workerHomeScreen/bottomNavifgation/bottom_navigation_worker.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:paniyaal/screens/workerHomeScreen/worker_home_screen.dart';
+
+
+class BottomNavigationWorker extends StatelessWidget {
+  BottomNavigationWorker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: WorkerHomeScreen.selectedIndexNotifierWorker,
+        builder: (BuildContext ctx, int updatedIndex, Widget? _) {
+          return BottomNavigationBar(
+            currentIndex: updatedIndex,
+            onTap: (newIndex) {
+              WorkerHomeScreen.selectedIndexNotifierWorker.value = newIndex;
+            },
+            selectedItemColor: Color(0xffdb3244),
+            unselectedItemColor: Colors.grey,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: 'Settings'),
+            ],
+          );
+        });
+  }
+}
--- lib/screens/workerHomeScreen/worker_home_screen.dart
+++ lib/screens/workerHomeScreen/worker_home_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'bottomNavifgation/bottom_navigation_worker.dart';
+
 class WorkerHomeScreen extends StatelessWidget {
   const WorkerHomeScreen({Key? key}) : super(key: key);
+  static ValueNotifier<int> selectedIndexNotifierWorker = ValueNotifier(0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Worker home screen '),),
+      bottomNavigationBar: BottomNavigationWorker(),
+      appBar: AppBar(
+        title: Text('Workers'),
+        centerTitle: true,
+        backgroundColor: Color(0xffdb3244),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
+        ],
+      ),
+      body: Center(
+        child: Text('Worker home screen '),
+      ),
     );
   }
 }
