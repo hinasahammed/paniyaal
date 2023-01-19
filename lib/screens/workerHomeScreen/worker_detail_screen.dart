ec8ea44a67e152e26fa5417e445a0d4466693769
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2023-01-19 07:11:45 -0800

    fg

--- lib/screens/workerHomeScreen/worker_detail_screen.dart
+++ lib/screens/workerHomeScreen/worker_detail_screen.dart
@@ -5,6 +5,41 @@ class WorkerDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit profile'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 15, top: 20, right: 15),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                            ),
+                          ],shape: BoxShape.circle,image: DecorationImage(image: AssetImage())),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
