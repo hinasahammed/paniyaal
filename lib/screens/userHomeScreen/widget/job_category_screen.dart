aec0e74e4d98dee2e3d31abc49a61a42f00ed3cc
Akshaykumart19 <115881925+Akshaykumart19@users.noreply.github.com>
2023-01-19 13:30:57 +0530

    lkhjgff

--- lib/screens/userHomeScreen/widget/job_category_screen.dart
+++ lib/screens/userHomeScreen/widget/job_category_screen.dart
@@ -256,6 +256,59 @@ class JobCategoryScreen extends StatelessWidget {
                     height: 15,
                   ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 155,
+                        height: 195,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image.asset(
+                                'assets/pest.jpg',
+                                fit: BoxFit.contain,
+                              ),
+                              Text(
+                                'Pest control\nservice',
+                                style: TextStyle(fontWeight: FontWeight.w500),textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 155,
+                        height: 195,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image.asset(
+                                'assets/technic.jpg',
+                                fit: BoxFit.contain,
+                              ),
+                              SizedBox(height: 5,),
+                              Text(
+                                'Technic repair',
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
                     children: [
                       Container(
                         width: 155,
--- lib/screens/workerHomeScreen/profile_screen.dart
+++ lib/screens/workerHomeScreen/profile_screen.dart
@@ -1,19 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../workerLoginScreen/worker_login_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  ProfileScreen({Key? key}) : super(key: key);
+  final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-        backgroundColor: Color(0xffdb3244),
-        centerTitle: true ,
-      ),
-      body: Center(
-        child: Text('Profile screen'),
-      ),
-    );
+      backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Profile'),
+          backgroundColor: Color(0xffdb3244),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: ListView(children: [
+              SizedBox(height: 30,),
+              Card(child: ListTile(
+                title: Text('Worker'),
+                leading:Icon(Icons.person),
+              ),),
+              Card(child: ListTile(
+                title: Text('Contact us'),
+                leading:Icon(Icons.contact_mail),
+              ),),
+              Card(child: ListTile(
+                title: Text('Logout'),
+                leading:Icon(Icons.logout),
+              ),),
+            ],)));
   }
 }
