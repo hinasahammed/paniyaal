34bac14fc0d29cec7e68b9c06577243dbf4e6edf
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2023-01-28 02:15:44 -0800

    hdfxhuy

--- lib/screens/jobTypeScreen/carpenter_screen.dart
+++ lib/screens/jobTypeScreen/carpenter_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class CarpenterScreen extends StatefulWidget {
@@ -8,14 +10,39 @@ class CarpenterScreen extends StatefulWidget {
 }
 
 class _CarpenterScreenState extends State<CarpenterScreen> {
+
+  final auth = FirebaseAuth.instance;
+  final _screenName = "Carpenter";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Carpenters'),
-        centerTitle: true,
-      ),
-      body: Text('Carpenter'),
+        appBar: AppBar(
+          backgroundColor: Color(0xffdb3244),
+          title: Text('Carpenters'),
+          centerTitle: true,
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("workersLogedIn")
+              .where("JobType", isEqualTo: _screenName)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return CircularProgressIndicator(strokeWidth: 3,color: Colors.white,);
+            } else {
+              return Column(
+                children: snapshot.data!.docs.map((document) {
+                  return Column(
+                    children: [
+
+                    ],
+                  );
+                }).toList(),
+              );
+            }
+          },
+        ),
     );
+    }
   }
-}
--- lib/screens/userHomeScreen/widget/settings_page.dart
+++ lib/screens/userHomeScreen/widget/settings_page.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:paniyaal/screens/typeSelectorScreen/type_selector_screen.dart';
+
+import '../../userPhoneVerificationScreen/user_phone_verification_screen.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key? key}) : super(key: key);
+   SettingsPage({Key? key}) : super(key: key);
+
+  final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +20,55 @@ class SettingsPage extends StatelessWidget {
         backgroundColor: Color(0xffdb3244),
         title: Text('Settings'),
       ),
+      body: Column(children: [
+        const SizedBox(
+          height: 25,
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Favourite'),
+            leading: Icon(Icons.favorite),
+            trailing: Icon(Icons.chevron_right),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (ctx) => TypeSelectorScreen()));
+            },
+            title: Text('Login as worker'),
+            leading: Icon(Icons.details),
+            trailing: Icon(Icons.chevron_right),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Contact us'),
+            leading: Icon(Icons.contact_mail),
+            trailing: Icon(Icons.chevron_right),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            onTap: () {
+              auth.signOut().then((value) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (ctx) => UserLoginScreen()));
+              }).onError((error, stackTrace) {
+                Fluttertoast.showToast(msg: error!.toString());
+              });
+            },
+            title: Text('Logout'),
+            leading: Icon(Icons.logout),
+            trailing: Icon(Icons.chevron_right),
+          ),
+        ),
+      ],),
     );
   }
 }
