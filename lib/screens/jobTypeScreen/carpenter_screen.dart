201e7a1628153672c28ec6425e68c46549e4df87
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2023-01-28 20:01:00 -0800

    sfgfdxh

--- lib/screens/jobTypeScreen/carpenter_screen.dart
+++ lib/screens/jobTypeScreen/carpenter_screen.dart
@@ -10,75 +10,97 @@ class CarpenterScreen extends StatefulWidget {
 }
 
 class _CarpenterScreenState extends State<CarpenterScreen> {
-
   final auth = FirebaseAuth.instance;
   final _screenName = "Carpenter";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xffdb3244),
-          title: Text('Carpenters'),
-          centerTitle: true,
-        ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection("workersLogedIn")
-              .where("jobType", isEqualTo: _screenName)
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return CircularProgressIndicator(strokeWidth: 3,color: Colors.white,);
-            } else {
-              return Column(
-                children: snapshot.data!.docs.map((document) {
-                  return Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Column(
-                      children: [
+      appBar: AppBar(
+        backgroundColor: Color(0xffdb3244),
+        title: Text('Carpenters'),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection("workersLogedIn")
+            .where("jobType", isEqualTo: _screenName)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator(
+              strokeWidth: 3,
+              color: Colors.white,
+            );
+          } else {
+            return Column(
+              children: snapshot.data!.docs.map((document) {
+                return Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: [
                       Container(
-                      width: double.infinity,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Image.network(
-                                  document["imageUrl"],
-                                  fit: BoxFit.contain,
-                                  height: 140,
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              IntrinsicHeight(
+                                child: Row(
+
+                                  children: [
+                                    Image.network(
+                                      document["imageUrl"],
+                                      fit: BoxFit.contain,
+                                      height: 140,
+                                    ),
+                                    VerticalDivider(
+                                      color: Colors.black,
+                                      thickness: 0.3,
+                                      endIndent: 5,
+                                    ),
+                                    Text(document["fullName"])
+                                  ],
                                 ),
-                                Text(
-                                    document["fullName"]
-                                )
-                              ],
-                            ),
-                            Divider(thickness: 2,),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                              IconButton(onPressed: (){}, icon: Icon(Icons.phone),),
-                              VerticalDivider(color: Colors.black,
-                                thickness: 2,),
-                              IconButton(onPressed: (){}, icon: Icon(Icons.book_online),),
-                            ],),
-                          ],
+                              ),
+                              Divider(
+                                thickness: 2,
+                              ),
+                              IntrinsicHeight(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.phone),
+                                    ),
+                                    VerticalDivider(
+                                      color: Colors.black,
+                                      thickness: 0.3,
+                                      endIndent: 5,
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.book_online),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-              );
-            }
-          },
-        ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            );
+          }
+        },
+      ),
     );
-    }
   }
+}
