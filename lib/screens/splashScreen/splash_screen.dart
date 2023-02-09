660ce49c1d3a78cd55f5f5769ba3fb3fbc3500a2
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2023-02-09 00:47:55 -0800

    cee

--- lib/screens/splashScreen/splash_screen.dart
+++ lib/screens/splashScreen/splash_screen.dart
@@ -1,11 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:paniyaal/screens/workerHomeScreen/worker_home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:paniyaal/screens/typeSelectorScreen/type_selector_screen.dart';
-import 'package:paniyaal/screens/userHomeScreen/user_home_screen.dart';
-import 'package:paniyaal/screens/userPhoneVerificationScreen/user_phone_verification_screen.dart';
-import 'package:paniyaal/screens/workerHomeScreen/worker_home_screen.dart';
 
+import '../userHomeScreen/user_home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -19,26 +17,44 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    gotoScreen(context);
+    // gotoScreen();
+    isLogedin();
   }
+  final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Image.asset('assets/logo.png'),
       ),
     );
   }
-}
-
-Future<void> gotoScreen(BuildContext context) async {
-  await Future.delayed(
-    Duration(seconds: 3),
-  );
-  Navigator.of(context).pushReplacement(
-    MaterialPageRoute(
-      builder: (ctx) => TypeSelectorScreen(),
-    ),
-  );
+  Future<void> gotoScreen() async {
+    await Future.delayed(
+      Duration(seconds: 3),
+    );
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (ctx) => TypeSelectorScreen(),
+      ),
+    );
+  }
+  isLogedin() async{
+    await Future.delayed(
+      Duration(seconds: 3),
+    );
+    final user = auth.currentUser;
+    if(user != null){
+      final email = user.email;
+      if(email!=null){
+        Navigator.push(context, MaterialPageRoute(builder: (ctx)=>WorkerHomeScreen()));
+      }else{
+        Navigator.push(context, MaterialPageRoute(builder: (ctx)=>UserHomeScreen()));
+      }
+}else{
+          Navigator.push(context, MaterialPageRoute(builder: (ctx)=>TypeSelectorScreen()));
+          }
+      }
 }
