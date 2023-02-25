69b3e0c4ee35ad40e1972455465a23e3da1650b7
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2023-02-25 06:38:26 -0800

    sf

--- lib/screens/jobTypeScreen/painter_screen.dart
+++ lib/screens/jobTypeScreen/painter_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -83,12 +84,10 @@ class _PainterScreenState extends State<PainterScreen> {
                                           child: ClipRRect(
                                             borderRadius:
                                             BorderRadius.circular(100),
-                                            child: Image.network(
-                                              document["imageUrl"],
-                                              width: 130,
-                                              height: 130,
+                                            child:CachedNetworkImage(imageUrl:  document["imageUrl"],width: 130,
+                                              height: 130,)
+
                                               fit: BoxFit.cover,
-                                            ),
                                           ),
                                         ),
                                       ],
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import firebase_messaging
 import firebase_storage
 import path_provider_macos
 import smart_auth
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -22,5 +23,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
